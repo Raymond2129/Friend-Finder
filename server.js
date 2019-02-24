ad60aef5b1c7a74bef16840384e403d1a200660f
Ray Ray <raymond2129@gmail.com>
2019-02-24 08:58:10 -0700

    file creation

---
+++ server.js
@@ -0,0 +1,16 @@
+//Dependencies
+var express = require("express");
+var path = require("path");
+
+//connection port
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+
+//Routes
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
+
+app.listen(PORT, function() {
+    console.log('App listening on PORT: ' + PORT);
+});
