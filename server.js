0258636b42a0e89d31117928862e7de7cc621229
Ray Ray <raymond2129@gmail.com>
2019-02-26 17:55:31 -0700

    found error in code and copied from a working point

--- server.js
+++ server.js
@@ -1,54 +1,43 @@
-//Dependencies:
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
 
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
 var app = express();
-var PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// BodyParser makes it possible for our server to interpret data sent to it.
+// The code below is pretty standard.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-//ROUTER
-require('./app/routing/api-routes.js')(app); 
-require('./app/routing/html-routes.js')(app);
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
 
-// Starts the server to begin listening
-app.listen(PORT, function () {
-  console.log('App listening on PORT: ' + PORT);
-});
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
+// ==============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// ==============================================================================
 
-
-// //Dependencies
-// var express = require("express");
-// var path = require("path");
-// var bodyParser = require("body-parser");
-
-// //connection port
-// var app = express();
-// var PORT = process.env.PORT || 3000;
-
-
-// //Routes
-// require('./app/routing/api-Routes.js/index.js')(app);
-// require('./app/routing/html-Routes.js/index.js')(app);
-
-
-// // Add the application routes
-// require('./app/routing/api-routes.js')(app); 
-// require('./app/routing/html-routes.js')(app);
-// //need to setup the express app to handle the data parsing
-// app.use(bodyParser.json());
-// app.use(express.static('app'));
-// app.use(bodyParser.urlencoded({ extended: true}));
-// app.use(bodyParser.text());
-// //app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
-
-// //App port to listen on locally
-// app.listen(PORT, function() {
-//     console.log('App listening on PORT: ' + PORT);
-// });
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});// Express server code here
