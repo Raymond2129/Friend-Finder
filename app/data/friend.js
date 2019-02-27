647f77a36ac055c5956a9939aaf1912d6b8c1289
Ray Ray <raymond2129@gmail.com>
2019-02-26 17:32:28 -0700

    updated the survey html

---
+++ app/data/friend.js
@@ -0,0 +1,24 @@
+
+    
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+  {
+    name: 'Raymond Province',
+    profilePic: 'https://avatars3.githubusercontent.com/u/42592534?s=460&v=4',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
--- server.js
+++ server.js
@@ -1,30 +1,54 @@
-//Dependencies
-var express = require("express");
-var path = require("path");
-var bodyParser = require("body-parser");
+//Dependencies:
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
 
-//connection port
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-
-//Routes
-require('./app/routing/apiRoutes.js')(app);
-require('./app/routing/htmlRoutes.js')(app);
-
-
-// Add the application routes
-require(path.join(__dirname, './app/routing/apiRoutes'))(app);
-require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
-
-//need to setup the express app to handle the data parsing
+// Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
-app.use(express.static('./app/public'));
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-//app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-//App port to listen on locally
-app.listen(PORT, function() {
-    console.log('App listening on PORT: ' + PORT);
+//ROUTER
+require('./app/routing/api-routes.js')(app); 
+require('./app/routing/html-routes.js')(app);
+
+// Starts the server to begin listening
+app.listen(PORT, function () {
+  console.log('App listening on PORT: ' + PORT);
 });
+
+
+
+// //Dependencies
+// var express = require("express");
+// var path = require("path");
+// var bodyParser = require("body-parser");
+
+// //connection port
+// var app = express();
+// var PORT = process.env.PORT || 3000;
+
+
+// //Routes
+// require('./app/routing/api-Routes.js/index.js')(app);
+// require('./app/routing/html-Routes.js/index.js')(app);
+
+
+// // Add the application routes
+// require('./app/routing/api-routes.js')(app); 
+// require('./app/routing/html-routes.js')(app);
+// //need to setup the express app to handle the data parsing
+// app.use(bodyParser.json());
+// app.use(express.static('app'));
+// app.use(bodyParser.urlencoded({ extended: true}));
+// app.use(bodyParser.text());
+// //app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
+
+// //App port to listen on locally
+// app.listen(PORT, function() {
+//     console.log('App listening on PORT: ' + PORT);
+// });
